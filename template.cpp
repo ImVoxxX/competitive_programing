e665fd633ca6a993b9afb1011bdf77056d932307
Nicolas Benedetti <nicolas007.b@gmail.com>
2026-06-23 14:25:48 -0300

    +template file for cpp problems

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+const ll inf = INT_MAX;
+const ld ep = 0.0000001;
+const ld pi = acos(-1.0);
+const ll md = 1000000007;
+
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--);
+
+    return 0;
+}
