259b2efd2d31dd4597ad81f4bac042d714e9a993
Nicolas Benedetti <nicolas007.b@gmail.com>
2026-06-23 11:01:33 -0300

    +TwoKnights problem solved

---
+++ cses/TwoKnights.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define lld long double
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+using namespace std;
+
+int main() {
+
+    ll n;
+    cin >> n;
+
+    cout << 0 << endl;
+    for (ll i = 2 ; i <= n ; i++) {
+      cout << (i * i) * (i * i - 1) / 2 - (4 * (i - 2) * (i - 1)) << endl;
+    }
+
+    return 0;
+}
